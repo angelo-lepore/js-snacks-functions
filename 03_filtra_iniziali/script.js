5759101649e1e3331faba91b873c849385931e60
angelo-lepore <angelo.lepore94@gmail.com>
2025-06-04 16:19:24 +0200

    finished-03-filtra-iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,22 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+function first_letters (names) {
+    const initial = ["A"]
+    const letters_A = [];
+    for (let i = 0; i < names.length; i++) {
+        if (names[i].startsWith(initial)) {
+            letters_A.push(names[i]);
+        }
+    }
+    return letters_A;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const letters_A = first_letters(names);
+console.log(letters_A);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]

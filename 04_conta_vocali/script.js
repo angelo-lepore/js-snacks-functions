df3ae28de7c10fd27945d7ca55f2f185fa67411b
angelo-lepore <angelo.lepore94@gmail.com>
2025-06-04 18:35:16 +0200

    finished-04-conta-vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,23 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
 
+function conta_vocali(text) {
+    const vocali = ['a', 'e', 'i', 'o', 'u'];
+    let vocali_presenti = []
+    for (let i = 0; i < text.length; i++) {
+        const lettere = text[i]
+        if (vocali.includes(lettere)) {
+            vocali_presenti.push(lettere)
+        }
+    }
+    return vocali_presenti.length;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const numeroVocali = conta_vocali(word);
+console.log(numeroVocali);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)

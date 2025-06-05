436cb4c9085aec8186fe35915bec1fa88f71db8f
angelo-lepore <angelo.lepore94@gmail.com>
2025-06-05 11:16:10 +0200

    finished-05-bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -7,12 +7,28 @@ e buonasera se è sera (oltre le 17)
 
 const name = 'Mario';
 
-
 // Dichiara la funzione qui.
 
+function greetings(name) {
+    const now = new Date ()
+    //now.setHours(18)
+    console.log(now);
+    const hour = now.getHours();
+
+    let message = ""
+    if (hour < 13) {
+        message = "Buongiorno"
+    } else if (hour <= 13 && hour < 17) {
+        message = "Buon pomeriggio"
+    } else {
+        message = "Buonasera"
+    }
+    return message + " " + firstName
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const firstName = "Angelo"
+console.log(greetings(firstName))
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.

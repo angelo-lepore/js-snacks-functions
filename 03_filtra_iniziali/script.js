984169522746dc4949cbf69b2fe9efa1119b0fb6
angelo-lepore <angelo.lepore94@gmail.com>
2025-06-05 10:41:15 +0200

    fix-03-filtra-iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,11 +4,10 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
-function first_letters (names) {
-    const initial = ["A"]
+function first_letters (names, char) {
     const letters_A = [];
     for (let i = 0; i < names.length; i++) {
-        if (names[i].startsWith(initial)) {
+        if (names[i].startsWith(char)) {
             letters_A.push(names[i]);
         }
     }
@@ -17,7 +16,8 @@ function first_letters (names) {
 
 // Invoca la funzione qui e stampa il risultato in console
 
-const letters_A = first_letters(names);
+const char_A = "A"
+const letters_A = first_letters(names, char_A);
 console.log(letters_A);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
